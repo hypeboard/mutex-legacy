655e4ffd85f551826be3fb24a9b453ed2a14c873
a <email@example.com>
2019-09-28 17:19:15 -0700

    bump

--- start_mutex_daemon.sh
+++ start_mutex_daemon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build/Darwin/release/release/bin/mutexd --rpc-bind-ip 0.0.0.0 --confirm-external-bind --block-sync-size=10 --in-peers=25 --out-peers=25 --log-level=1
+./build/Darwin/master/release/bin/mutexd --limit-rate=1000000 --out-peers=30 --in-peers=30 --rpc-bind-ip=0.0.0.0 --confirm-external-bind --rpc-bind-port=26943 --p2p-bind-ip=0.0.0.0 --log-level=0 --add-peer=35.172.245.22:26942 --add-peer=35.168.54.62:26942 --add-peer=3.90.100.66:26942 --add-peer=34.232.227.76:26942 --add-peer=3.210.60.30:26942 --disable-dns-checkpoints
