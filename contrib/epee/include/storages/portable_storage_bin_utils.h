92487dd56388603e9418a92eb8dc7ddca2ff0d17
a <email@example.com>
2019-10-22 02:34:10 -0700

    epee rebase

--- contrib/epee/include/storages/portable_storage_bin_utils.h
+++ contrib/epee/include/storages/portable_storage_bin_utils.h
@@ -1,21 +1,21 @@
-// Copyright (c) 2018, The Monero Project
-//
+// Copyright (c) 2019, The Monero Project
+// 
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-//
+// 
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-//
+// 
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-//
+// 
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-//
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -26,40 +26,21 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#pragma once
+#pragma once 
 
-#include <boost/utility/string_ref.hpp>
-#include <cstdint>
+#include "int-util.h"
 
-namespace epee
-{
-namespace net_utils
-{
-	enum class address_type : std::uint8_t
-	{
-		// Do not change values, this will break serialization
-		invalid = 0,
-		ipv4 = 1,
-		ipv6 = 2,
-		i2p = 3,
-		tor = 4
-	};
+template<typename T> T convert_swapper(T t) { return t; }
+template<> inline uint16_t convert_swapper(uint16_t t) { return SWAP16LE(t); }
+template<> inline int16_t convert_swapper(int16_t t) { return SWAP16LE((uint16_t&)t); }
+template<> inline uint32_t convert_swapper(uint32_t t) { return SWAP32LE(t); }
+template<> inline int32_t convert_swapper(int32_t t) { return SWAP32LE((uint32_t&)t); }
+template<> inline uint64_t convert_swapper(uint64_t t) { return SWAP64LE(t); }
+template<> inline int64_t convert_swapper(int64_t t) { return SWAP64LE((uint64_t&)t); }
+template<> inline double convert_swapper(double t) { union { uint64_t u; double d; } u; u.d = t; u.u = SWAP64LE(u.u); return u.d; }
 
-	enum class zone : std::uint8_t
-	{
-		invalid = 0,
-		public_ = 1, // public is keyword
-		i2p = 2,     // order from here changes priority of selection for origin TXes
-		tor = 3
-	};
-
-	// implementations in src/net_utils_base.cpp
-
-	//! \return String name of zone or "invalid" on error.
-	const char* zone_to_string(zone value) noexcept;
-
-	//! \return `zone` enum of `value` or `zone::invalid` on error.
-	zone zone_from_string(boost::string_ref value) noexcept;
-} // net_utils
-} // epee
-
+#if BYTE_ORDER == BIG_ENDIAN
+#define CONVERT_POD(x) convert_swapper(x)
+#else
+#define CONVERT_POD(x) (x)
+#endif
